32818da317c15dc0465d56bb1cc2b524e5c104ef
master8848 <jobarsanjel2@gmail.com>
2022-07-29 14:54:44 +0545

    seaction

--- src/components/sidebar-activities/admin/data-setup/section/add-section/AddSection.js
+++ src/components/sidebar-activities/admin/data-setup/section/add-section/AddSection.js
@@ -28,13 +28,13 @@ const AddSection = () => {
         <div className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 gap-4">
           <div>
             <label className="my-6 text-sm" htmlFor="Student Id">
-              University/Board*
+              Section*
             </label>
             <br />
             <input
               className=" mt-[6px] w-full p- rounded  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400    text-primary-grey-700 text-sm"
               type="number"
-              placeholder="National Education Board"
+              placeholder="A"
             />
           </div>
         </div>
@@ -42,13 +42,13 @@ const AddSection = () => {
           <div className="md:flex-row w-fit flex flex-col my-6 ml-auto">
             <div className=" w-fit my-auto">
               <Link
-                to="/admin/data-setup/university-board"
+                to="/admin/data-setup/section"
                 className="bg-primary-grey-50 text-primary-grey-700 hover: focus:outline-none focus:ring- focus:ring-offset-2 sm:w-auto inline-flex items-center justify-center px-4 py-3 mr-3 text-sm font-medium border border-transparent rounded-md shadow-sm"
               >
                 Cancel
               </Link>
               <Link
-                to="/admin/data-setup/university-board"
+                to="/admin/data-setup/section"
                 className="bg-primary-btn hover: focus:outline-none focus:ring- focus:ring-offset-2 sm:w-auto inline-flex items-center justify-center px-4 py-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm"
               >
                 Next
