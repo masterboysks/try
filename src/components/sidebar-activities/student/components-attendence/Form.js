74f65223807032f6db3d9eb495aeb6e032dc90cb
master8848 <jobarsanjel2@gmail.com>
2022-08-09 12:43:54 +0545

    changed ststus in student attendence

--- src/components/sidebar-activities/student/components-attendence/Form.js
+++ src/components/sidebar-activities/student/components-attendence/Form.js
@@ -2,11 +2,11 @@ import React from "react";
 import Search from "@mui/icons-material/SearchOutlined";
 const Form = () => {
   return (
-    <div className="w-full p-4 my-6 rounded-md shadow ring-black ring-opacity-5 ring-1 form-solid mb-14">
-      <form className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="ring-black ring-opacity-5 ring-1 form-solid mb-14 w-full p-4 my-6 rounded-md shadow">
+      <form className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 gap-4">
         <div className="">
           <label className="my-6 text-sm" htmlFor="Status">
-            Status
+            Level
           </label>
           <br />
           <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
@@ -15,7 +15,7 @@ const Form = () => {
         </div>
         <div className="">
           <label className="my-6 text-sm" htmlFor="Status">
-            Status
+            Class/Semester
           </label>
           <br />
           <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
@@ -24,7 +24,7 @@ const Form = () => {
         </div>
         <div className="">
           <label className="my-6 text-sm" htmlFor="Status">
-            Status
+            Faculty
           </label>
           <br />
           <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
@@ -33,7 +33,7 @@ const Form = () => {
         </div>
         <div className="">
           <label className="my-6 text-sm" htmlFor="Status">
-            Status
+            Section
           </label>
           <br />
           <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
@@ -41,7 +41,7 @@ const Form = () => {
           </select>
         </div>
         <div className="col-span-full lg:col-span-2 xl:col-span-full">
-          <div className="flex items-center justify-center p-2 ml-auto text-sm font-medium text-white border border-transparent rounded-md shadow-sm cursor-pointer bg-primary-btn h-fit w-fit hover: focus:outline-none focus:ring- focus:ring-offset-2">
+          <div className="bg-primary-btn h-fit w-fit hover: focus:outline-none focus:ring- focus:ring-offset-2 flex items-center justify-center p-2 ml-auto text-sm font-medium text-white border border-transparent rounded-md shadow-sm cursor-pointer">
             <Search />
           </div>
         </div>
